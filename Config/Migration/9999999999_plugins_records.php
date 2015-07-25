582f0e313edd6d1559b98d9b8aeb4ac4181f2702
s-nakajima <nakajimashouhei@gmail.com>
2015-07-25 20:18:03 +0900

    Fix migration file name

---
+++ Config/Migration/9999999999_plugins_records.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Add plugin migration
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsMigration', 'NetCommons.Config/Migration');
+
+/**
+ * Add plugin migration
+ *
+ * @package NetCommons\PluginManager\Config\Migration
+ */
+class PluginRecords extends NetCommonsMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'plugin_records';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(),
+		'down' => array(),
+	);
+
+/**
+ * plugin data
+ *
+ * @var array $migration
+ */
+	public $records = array(
+		'Plugin' => array(
+			'key' => 'plugin_manager',
+			'namespace' => 'netcommons/plugin-manager',
+			'name' => 'Plugin Manager',
+			'type' => 2,
+			'default_action' => 'plugin_manager/index',
+			'default_setting_action' => '',
+			'weight' => 5,
+		),
+		'PluginsRole' => array(
+			array(
+				'role_key' => 'system_administrator',
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		$this->loadModels([
+			'Plugin' => 'PluginManager.Plugin',
+		]);
+
+		if ($direction === 'down') {
+			$this->Plugin->uninstallPlugin($this->records);
+		} else {
+			$this->Plugin->installPlugin($this->records);
+		}
+		return true;
+	}
+}
