635f187c1035c45f143191458209769a24fb6beb
s-nakajima <nakajimashouhei@gmail.com>
2016-10-16 23:37:32 +0900

    phpunitエラー修正

--- Test/Case/AllPluginManagerTest.php
+++ Test/Case/AllPluginManagerTest.php
@@ -29,17 +29,6 @@ class AllPluginManagerTest extends NetCommonsTestSuite {
 		$plugin = preg_replace('/^All([\w]+)Test$/', '$1', __CLASS__);
 		$suite = new NetCommonsTestSuite(sprintf('All %s Plugin tests', $plugin));
 		$suite->addTestDirectoryRecursive(CakePlugin::path($plugin) . 'Test' . DS . 'Case');
-
-		$Folder = new Folder(CakePlugin::path($plugin) . 'Test' . DS . 'Case');
-		$files = $Folder->tree(null, true, 'files');
-		foreach ($files as $file) {
-			if (preg_match('/\/All([\w]+)Test\.php$/', $file)) {
-				continue;
-			}
-			if (substr($file, -8) === 'Test.php') {
-				var_dump($file);
-			}
-		}
 		return $suite;
 	}
 }
