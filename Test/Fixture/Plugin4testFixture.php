29cf9059b90bedf37196fc5f468c32e0d658d1db
s-nakajima <nakajimashouhei@gmail.com>
2017-08-28 12:28:48 +0900

    Plugin::getPlugins()のテスト追加

--- Test/Fixture/Plugin4testFixture.php
+++ Test/Fixture/Plugin4testFixture.php
@@ -38,6 +38,7 @@ class Plugin4testFixture extends PluginFixture {
  * @var array
  */
 	public $records = array(
+		//プラグイン管理
 		array(
 			'id' => '1',
 			'language_id' => '1',
@@ -45,7 +46,7 @@ class Plugin4testFixture extends PluginFixture {
 			'name' => 'Plugin Manager',
 			'namespace' => 'netcommons/plugin-manager',
 			'weight' => '1',
-			'type' => '1',
+			'type' => '4',
 			'default_action' => '',
 			'default_setting_action' => '',
 		),
@@ -56,7 +57,91 @@ class Plugin4testFixture extends PluginFixture {
 			'name' => 'Plugin Manager',
 			'namespace' => 'netcommons/plugin-manager',
 			'weight' => '1',
+			'type' => '4',
+			'default_action' => '',
+			'default_setting_action' => '',
+		),
+		//システム管理
+		array(
+			'id' => '3',
+			'language_id' => '2',
+			'key' => 'system_manager',
+			'name' => 'System Manager',
+			'namespace' => 'netcommons/system-manager',
+			'weight' => '2',
+			'type' => '4',
+			'default_action' => '',
+			'default_setting_action' => '',
+		),
+		//会員管理
+		array(
+			'id' => '4',
+			'language_id' => '2',
+			'key' => 'user_manger',
+			'name' => 'User Manager',
+			'namespace' => 'netcommons/user-manager',
+			'weight' => '1',
+			'type' => '3',
+			'default_action' => '',
+			'default_setting_action' => '',
+		),
+		//ルーム管理
+		array(
+			'id' => '5',
+			'language_id' => '2',
+			'key' => 'rooms',
+			'name' => 'Room Manager',
+			'namespace' => 'netcommons/rooms',
+			'weight' => '2',
+			'type' => '3',
+			'default_action' => '',
+			'default_setting_action' => '',
+		),
+		//権限管理
+		array(
+			'id' => '6',
+			'language_id' => '2',
+			'key' => 'user_roles',
+			'name' => 'UserRole Manager',
+			'namespace' => 'netcommons/user-roles',
+			'weight' => '3',
+			'type' => '3',
+			'default_action' => '',
+			'default_setting_action' => '',
+		),
+		//お知らせ
+		array(
+			'id' => '7',
+			'language_id' => '2',
+			'key' => 'announcements',
+			'name' => 'Announcements',
+			'namespace' => 'netcommons/announcements',
+			'weight' => '1',
 			'type' => '1',
+			'default_action' => '',
+			'default_setting_action' => '',
+		),
+		//掲示板
+		array(
+			'id' => '8',
+			'language_id' => '2',
+			'key' => 'bbses',
+			'name' => 'BBS',
+			'namespace' => 'netcommons/bbses',
+			'weight' => '2',
+			'type' => '1',
+			'default_action' => '',
+			'default_setting_action' => '',
+		),
+		//NetCommons
+		array(
+			'id' => '9',
+			'language_id' => '0',
+			'key' => 'net-commons',
+			'name' => 'NetCommons',
+			'namespace' => 'netcommons/net-commons',
+			'weight' => '1',
+			'type' => '0',
 			'default_action' => '',
 			'default_setting_action' => '',
 		),
---
+++ TestSuite/PluginManagerGetTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * PluginManagerGetTest TestCase
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+//@codeCoverageIgnoreStart;
+App::uses('NetCommonsGetTest', 'NetCommons.TestSuite');
+//@codeCoverageIgnoreEnd;
+
+/**
+ * PluginManagerGetTest TestCase
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\PluginManager\TestSuite
+ * @codeCoverageIgnore
+ */
+abstract class PluginManagerGetTest extends NetCommonsGetTest {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	private $__fixtures = array(
+		'plugin.plugin_manager.plugin4test',
+		'plugin.plugin_manager.plugins_role',
+		'plugin.plugin_manager.plugins_room',
+	);
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'plugin_manager';
+
+/**
+ * Fixtures load
+ *
+ * @param string $name The name parameter on PHPUnit_Framework_TestCase::__construct()
+ * @param array  $data The data parameter on PHPUnit_Framework_TestCase::__construct()
+ * @param string $dataName The dataName parameter on PHPUnit_Framework_TestCase::__construct()
+ * @return void
+ */
+	public function __construct($name = null, array $data = array(), $dataName = '') {
+		if (! isset($this->fixtures)) {
+			$this->fixtures = array();
+		}
+		$this->fixtures = array_merge($this->__fixtures, $this->fixtures);
+		parent::__construct($name, $data, $dataName);
+	}
+
+}
